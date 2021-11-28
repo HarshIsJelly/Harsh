876df147fbad6a8eed4548c7d47b3415be95b471
HarshIsJelly <95181815+HarshIsJelly@users.noreply.github.com>
2021-11-28 20:04:15 +0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,15 +1,37 @@
 
 function setup() {
   createCanvas(400,400);
-  var box = createSprite(200,200,30,30);
+  background(51);
+  box = createSprite(200,200,30,30);
+
 }
 
 function draw() 
 {
-  background(30);
-drawSprites();
+
+  
+
+  if (keyIsDown(LEFT_ARROW)) 
+  {
+    background("blue");
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    background("pink");
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    background("green");
+  }
+
+if(keyIsDown(RIGHT_ARROW)){
+   background("yellow")
 }
 
-
-
+  drawSprites();
+}
 
